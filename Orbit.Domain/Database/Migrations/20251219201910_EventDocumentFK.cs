84f7d0fbd2b49184b777b02b75e611b89ed2a677
Benjamin Regan <35966588+Bregann@users.noreply.github.com>
2025-12-19 20:19:55 +0000

    migration

---
+++ Orbit.Domain/Database/Migrations/20251219201910_EventDocumentFK.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Orbit.Domain.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class EventDocumentFK : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DocumentId",
+                table: "CalendarEvents",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CalendarEvents_DocumentId",
+                table: "CalendarEvents",
+                column: "DocumentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CalendarEvents_Documents_DocumentId",
+                table: "CalendarEvents",
+                column: "DocumentId",
+                principalTable: "Documents",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CalendarEvents_Documents_DocumentId",
+                table: "CalendarEvents");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CalendarEvents_DocumentId",
+                table: "CalendarEvents");
+
+            migrationBuilder.DropColumn(
+                name: "DocumentId",
+                table: "CalendarEvents");
+        }
+    }
+}
