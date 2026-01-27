1d7ef5d00b1bccb3f102c33f360bb879180537a8
Benjamin Regan <35966588+Bregann@users.noreply.github.com>
2026-01-27 21:32:31 +0000

    ns&i fix

--- Orbit.Domain/Services/Finance/BankService.cs
+++ Orbit.Domain/Services/Finance/BankService.cs
@@ -46,7 +46,7 @@ namespace Orbit.Domain.Services.Finance
                 // Skip excluded merchants
                 if (merchantName == "Unknown" ||
                     merchantName.Contains("MONZO CARD MONZO VIA MOBILE") ||
-                    merchantName.Contains("NSANDI"))
+                    merchantName.Contains("NS&I"))
                 {
                     continue;
                 }
